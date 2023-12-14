3e11207e81ce9ee581ee8ae0715b668b4fc6af50
Iconicyansh <150763113+Iconicyansh@users.noreply.github.com>
2023-12-14 10:42:20 +0530

    Update app.js

--- particles/app.js
+++ particles/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#c30010"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#c30010",
         "opacity": 0.4,
         "width": 1
       },
